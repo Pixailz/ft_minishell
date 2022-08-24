3b667a8eb360984c1b10a0dba79c375a5174712a
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-08-24 22:14:38 +0200

    :construction: add little base

--- src/minishell.c
+++ src/minishell.c
@@ -6,14 +6,17 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 23:57:29 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/24 20:50:59 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/08/24 21:06:26 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "blank.h"
+#include "minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	if (argc != 1)
+		return (ft_error("no arguments needed", 1));
+	main_loop();
 	ft_printf("Hello World!\n");
 	return (0);
 }
