b1f7700317961091c691eb697927373eb48ca5c8
Pixailz <pixailz1@gmail.com>
2022-08-31 16:44:13 +0200

    :package: test/ - add test folder, for some test

---
+++ test/test.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 16:10:10 by brda-sil          #+#    #+#             */
+/*   Updated: 2022/08/31 16:12:10 by brda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+void	push_string(char **str, int n)
+{
+	int	counter;
+
+	counter = 0;
+	while (counter < n)
+	{
+		(*str)++;
+		counter++;
+	}
+}
+
+int main(void)
+{
+	char	*test;
+
+	test = strdup("this is a test");
+	printf("%s\n", test);
+	push_string(&test, 3);
+	printf("%s\n", test);
+}
