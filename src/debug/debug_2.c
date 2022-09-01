60fa8ded7d6987ae2b4fe6008dea51d192904710
Pixailz <pixailz1@gmail.com>
2022-09-01 03:38:20 +0200

    :package: src - new file   - debug_2.c, because never enought debuging   - get_path to retrieve PATH variable from envp

---
+++ src/debug/debug_2.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 01:32:17 by brda-sil          #+#    #+#             */
+/*   Updated: 2022/09/01 02:48:07 by brda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	debug_parse_arg(t_cmd *cmd)
+{
+	int	counter;
+
+	ft_printf_fd(LOG_FD, "   %s cmd->name\t\t\t[%s]\n", GREEN_PLUS, cmd->name);
+	if (cmd->path)
+		ft_printf_fd(LOG_FD, "   %s cmd->path\t\t\t[%s]\n", GREEN_PLUS, \
+																	cmd->path);
+	if (cmd->have_arg)
+	{
+		counter = 0;
+		ft_printf_fd(LOG_FD, "   %s cmd->have_arg\t\tTrue\n", GREEN_PLUS);
+		while (cmd->arg[counter])
+		{
+			ft_printf_fd(LOG_FD, "   %s cmd->arg[%d]\t\t[%s]\n", GREEN_PLUS, \
+													counter, cmd->arg[counter]);
+			counter++;
+		}
+	}
+	else
+		ft_printf_fd(LOG_FD, "   %s cmd->have_arg\t\tFalse\n", GREEN_PLUS);
+}
