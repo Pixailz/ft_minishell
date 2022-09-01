5c8d0f22398dd086202845aaecead417a8eb403f
Pixailz <pixailz1@gmail.com>
2022-09-01 03:36:16 +0200

    :package: src/minishell.c - revert back to main main

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 23:57:29 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/31 18:49:17 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/09/01 02:40:35 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,15 @@ char	**do_something_with_argv(char **argv)
 	return (argv);
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	char	**string_splitted;
-	int		counter;
-	char	*string;
-	int		choice;
+	t_main	config;
 
-	if (argc != 2)
-		return (ft_error("args", 1));
-	// string = ft_strdup("echo -n 'test'");
-	// string = ft_strdup("echo -n '  test  '");
-	// string = ft_strdup("echo   ''\"\"");
-	// string = ft_strdup("echo \"-n\" ' test '");
-	// string = ft_strdup("echo \"\"n''");
-	// string = ft_strdup("test -f ./file.c && echo '  pass ' || echo '  failed '   a'");
-	// string = ft_strdup("ec'ho 'salut");
-	string_splitted = ft_splitb(argv[1], ' ', "'\"");
-	ft_printf("string    \t-> [%s]\n", argv[1]);
-	counter = 0;
-	while (string_splitted[counter])
-	{
-		ft_printf("splitted[%d]\t-> [%s]\n", counter, string_splitted[counter]);
-		counter++;
-	}
+	if (argc != 1)
+		return (ft_error("no arguments needed", 1));
+	do_something_with_argv(argv);
+	init_entry(&config, envp);
+	main_loop(&config);
+	free_entry(&config);
+	return (0);
 }
-
-// int	main(int argc, char **argv)
-// {
-// 	t_main	config;
-
-// 	if (argc != 1)
-// 		return (ft_error("no arguments needed", 1));
-// 	do_something_with_argv(argv);
-// 	init_entry(&config);
-// 	main_loop(&config);
-// 	free_entry(&config);
-// 	return (0);
-// }
