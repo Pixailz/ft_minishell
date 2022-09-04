3bb238fa93afb69c38d0752ef722827034c70474
Pixailz <pixailz1@gmail.com>
2022-09-04 05:11:46 +0200

    :package: src/debug/debug_(1|2).c - fix padding

--- src/debug/debug_1.c
+++ src/debug/debug_1.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 05:20:53 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/09/01 01:47:11 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/09/04 03:35:34 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ void	debug_init(t_main *config)
 {
 	int	counter;
 
-	ft_printf_fd(LOG_FD, "%s initial prompt\t\t[%s]\n", GREEN_PLUS, \
+	ft_printf_fd(LOG_FD, "%s initial prompt\t\t\t[%s]\n", GREEN_PLUS, \
 																config->prompt);
 	counter = 0;
 	while (config->env_path[counter])
 	{
-		ft_printf_fd(LOG_FD, "   %s path[%d]\t\t\t[%s]\n", GREEN_PLUS, \
+		ft_printf_fd(LOG_FD, "   %s path[%d]\t\t\t\t[%s]\n", GREEN_PLUS, \
 											counter, config->env_path[counter]);
 		counter++;
 	}
@@ -31,7 +31,7 @@ void	debug_line_buffer(char *line)
 {
 	static int	id = 1;
 
-	ft_printf_fd(LOG_FD, "%s line_buffer(%d)\t\t[%s]\n", GREEN_PLUS, id++, line);
+	ft_printf_fd(LOG_FD, "%s line_buffer(%d)\t\t\t[%s]\n", GREEN_PLUS, id++, line);
 }
 
 int	debug_catched_signal(int sig)
@@ -51,9 +51,9 @@ int	debug_catched_signal(int sig)
 
 void	debug_init_cmd(t_main *config)
 {
-	ft_printf_fd(LOG_FD, "   %s nb_quote\t\t\t%d\n", GREEN_PLUS, \
+	ft_printf_fd(LOG_FD, "   %s nb_quote\t\t\t\t%d\n", GREEN_PLUS, \
 														config->cmd->nb_quote);
-	ft_printf_fd(LOG_FD, "   %s nb_dquote\t\t%d\n", GREEN_PLUS, \
+	ft_printf_fd(LOG_FD, "   %s nb_dquote\t\t\t%d\n", GREEN_PLUS, \
 														config->cmd->nb_dquote);
 }
 
--- src/debug/debug_2.c
+++ src/debug/debug_2.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 01:32:17 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/09/01 02:48:07 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/09/04 03:35:12 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	debug_parse_arg(t_cmd *cmd)
 		ft_printf_fd(LOG_FD, "   %s cmd->have_arg\t\tTrue\n", GREEN_PLUS);
 		while (cmd->arg[counter])
 		{
-			ft_printf_fd(LOG_FD, "   %s cmd->arg[%d]\t\t[%s]\n", GREEN_PLUS, \
+			ft_printf_fd(LOG_FD, "   %s cmd->arg[%d]\t\t\t[%s]\n", GREEN_PLUS, \
 													counter, cmd->arg[counter]);
 			counter++;
 		}
