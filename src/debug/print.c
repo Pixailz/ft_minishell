9802eefe2bfc70ddd76e6b9c3fef36490d1f7921
Pixailz <pixailz1@gmail.com>
2022-09-21 23:34:47 +0200

    :package: src/debug/print.c - make function don't flag anymore

--- src/debug/print.c
+++ src/debug/print.c
@@ -6,16 +6,19 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 20:20:23 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/09/21 05:40:47 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/09/21 21:07:15 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	debug_print(int type, void *ptr)
+void	debug_print_entry(int type, void *ptr)
 {
 	if (!DEBUG)
 		return ;
+	if (type == NONE)
+		return ;
+	return ((void)ptr);
 }
 
 void	debug_print_question_mark(t_main *config)
