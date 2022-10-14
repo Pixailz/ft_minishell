6bd154a8e8bb11c7f8eea30b14ecedfe2e2e17e4
Pixailz <pixailz1@gmail.com>
2022-10-14 14:26:38 +0200

    :package: exit.c - fix first non numeric argument and multiple args

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 05:35:55 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/10/02 00:11:03 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/10/14 14:23:56 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,19 @@ int	exit_check_shlvl(t_main *config)
 		return (False);
 	}
 	return (True);
+}
+
+void	builtin_exit_multiple_args(t_cmd *cmd, t_main *config)
+{
+	if (!ft_sisdigit(cmd->command[1]))
+	{
+		ft_printf("minishell: exit: %s: numeric argument required\n", \
+			cmd->command[1]);
+		free_config_entry(config);
+		exit(2);
+	}
+	else
+		ft_printf("minishell: exit: too many arguments\n", cmd->command[1]);
 }
 
 void	builtin_exit_post_exec(t_cmd *cmd, t_main *config)
@@ -39,7 +52,7 @@ void	builtin_exit_post_exec(t_cmd *cmd, t_main *config)
 		}
 	}
 	if (cmd->return_value == 2)
-		ft_printf("minishell: exit: too many arguments\n", cmd->command[1]);
+		builtin_exit_multiple_args(cmd, config);
 	else
 	{
 		if (exit_check_shlvl(config))
